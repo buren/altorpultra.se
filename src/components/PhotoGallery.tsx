1d591c4b9d340ba545e0607dc1f46cbed2ee2ba8
Jacob Burenstam <burenstam@gmail.com>
2024-10-21 15:04:40 +0200

    Add PhotoGallery component

---
+++ src/components/PhotoGallery.tsx
@@ -0,0 +1,8 @@
+import { StaticImageData } from "next/image";
+import { RowsPhotoAlbum } from "react-photo-album";
+
+export default function PhotoGallery({ images }: { images: StaticImageData[] }) {
+  const photos = images.map(({ src, height, width, }) => ({ src, height, width, }));
+
+  return <RowsPhotoAlbum photos={photos} />;
+}
